ea370dc1cb69233cdabe6c11b5f3fbb929176b1f
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-06-01 17:28:04 +0200

    new

--- Auction.hs
+++ Auction.hs
@@ -21,35 +21,57 @@ mkAuction turn = Auction [] turn
 
 -- private function. Assumes the auction is constructed correctly.
 result :: Auction -> Maybe Contract
+
 result (Auction [(_, Pass), (_, Pass), (_, Pass), (_, Pass)] _) = Just FourPasses
+
 result (Auction ((_, Pass):(_, Pass):(_, Pass):calls) _) =
-  Just $ Contract level strain penalty direction' where
-  (_, call):_ = calls
-  penalty = case call of Pen pen -> Just pen; _ -> Nothing
-  toLastBidxs = toLastBid calls
-  (direction, (Bid level strain)):_ = toLastBidxs
-  (direction', _) = firstDeclarer direction strain toLastBidxs
+  let
+    (_, call):_ = calls -- by properties of the auction
+    penalty = case call of Pen pen -> Just pen; otherwise -> Nothing
+    toLastBid =
+      dropWhile(\(_, call) -> case call of (Bid _ _) -> False; _ -> True) calls
+
+    (direction, (Bid level strain)):_ = toLastBid
+    direction' =  fst $ firstDeclarer direction strain toLastBid
+  in Just $ Contract level strain penalty direction'
+
 result _ = Nothing
+
 firstDeclarer direction' strain' calls' = last $ 
   filter(\(direction, call) -> (isPartner direction' direction)
   && case call of
     Bid _ strain -> strain == strain'
     _ -> False
   ) calls'
-toLastBid calls' = dropWhile(\(_, call) -> case call of (Bid _ _) -> False; _ -> True) calls'
 
 
--- this function assumes that the auction was constructed correctly
+-- thisfunction assumes that the auction was constructed correctly
 availableCalls :: Auction -> [Call]
 availableCalls (Auction calls turn) =
   case noPasses of
-  [] -> Pass:[Bid level strain | level <- [minBound..maxBound], strain <- [minBound..maxBound]]
-  (direction, strain):_ -> if isPartner direction turn
-    then undefined
-    else undefined
+
+    [] -> Pass:[Bid level strain 
+      | level <- [minBound..maxBound], strain <- [minBound..maxBound]]
+
+    (direction, call):_ -> if isPartner direction turn
+      then 
+        let
+          (_, Bid level strain):_ = findLastBid noPasses
+        in Pass:(higherBids level strain)
+      else
+        let (_, Bid level strain):_ = findLastBid noPasses
+        in case call of
+          Bid _ _ -> Pass:(Pen Double):(higherBids level strain)
+          Pen Double -> Pass:(Pen Redouble):(higherBids level strain)
+          Pen Redouble -> Pass:(higherBids level strain)
+      
   where
   noPasses = dropWhile (\(_, call) -> case call of Pass -> True; _ -> False) calls
-
+  higherBids level strain = 
+    [ Bid level strain' | strain' <- [strain..maxBound], strain' /= strain] ++ 
+    [Bid level' strain' | strain' <-[minBound..maxBound], level' <- [minBound..maxBound], level' > level]
+  findLastBid calls' = dropWhile (\(_, call) -> case call of Pass -> True; _ -> False) calls'
 --SANDBOX---------------
 
+higherBids level strain = [(level, strain') | strain' <- [strain..maxBound], strain' /= strain] ++ [(level', strain') | strain' <-[minBound..maxBound], level' <- [minBound..maxBound], level' > level]
 -- runAuction :: StateT Auction (ReaderT Convention (IO Call)) Play
--- manager.sh
+++ manager.sh
@@ -5,8 +5,12 @@ if [ "$1" == "clear" ]; then
   rm -rf *PF2%projekt*
 elif [ "$1" == "compile" ]; then
   ghc -hidir hi -odir o main.hs
+elif [ "$1" == "git" ]; then
+  git apdejtujkurwa
+  git takkurwa $2
+  git nakurwiaj
 else
-  echo "Usage: $0 {clear|compile}"
+  echo "Usage: $0 {clear|compile|git <$2>}"
   exit 1
 fi
 
