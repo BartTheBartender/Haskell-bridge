ef8e44baff01c9f3ce7ae9b30f3478cbcf637c43
Bartosz Furmanek <126062736+BartTheBartender@users.noreply.github.com>
2024-06-01 17:29:42 +0200

    ready to write runAuction

--- manager.sh
+++ manager.sh
@@ -5,10 +5,6 @@ if [ "$1" == "clear" ]; then
   rm -rf *PF2%projekt*
 elif [ "$1" == "compile" ]; then
   ghc -hidir hi -odir o main.hs
-elif [ "$1" == "git" ]; then
-  git apdejtujkurwa
-  git takkurwa $2
-  git nakurwiaj
 else
   echo "Usage: $0 {clear|compile|git <$2>}"
   exit 1
